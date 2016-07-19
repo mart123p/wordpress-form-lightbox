86419ffb5d847b47386049145fe0cbd4b0e93b23
Martin Pouliot <contact@martinpouliot.ca>
2016-07-18 20:52:39 -0400

    Fixed a security issue where an attacker could change any option of the wordpress database.

---
+++ Readme.md
@@ -0,0 +1,2 @@
+##Form-lightbox##
+This is a vulnerable plugin. Even though it is old I have seen myself attacks using this plugin as an entry point. I have a applied the requiered corrections in order to secure it.
--- ajax.php
+++ ajax.php
@@ -10,33 +10,56 @@ $_POST = array_map( 'stripslashes_deep', $_POST );
 
 $action = isset( $_POST['action'] ) ? $_POST['action'] : false;
 
+
+function validateInput($optionName){
+	$requests =
+		['_colorbox_style',
+		'_colorbox_transition',
+		'_colorbox_speed',
+		'_colorbox_scrolling',
+		'_colorbox_opacity',
+		'_colorbox_returnFocus',
+		'_colorbox_fastIframe',
+		'_colorbox_closeBtn',
+		'_colorbox_escKey',
+		'_menu_object_nos',
+		'_menu_obj_'];
+	return (in_array($optionName,$requests));
+
+}
+
 switch ( $action ) {
 
-	case 'update_content' : 
-	
-			update_option( $_POST['update'], $_POST['value']); 
-			$start = false;
-			$new_options = array();
-			foreach($flb->options() as $value){
-				if($start)
-					$new_options[] = $value;
-					
-				if($value['type']=='open_ajax' && $value['id'] == $_POST['ajax'])
-					$start = true;
-					
-				if($value['type']=='close_ajax' && $value['id'] == $_POST['ajax'] . "_close")
-					break;
+	case 'update_content' :
+			if(validateInput( $_POST['update'])) {
+				update_option($_POST['update'], $_POST['value']);
+				$start = false;
+				$new_options = array();
+				foreach ($flb->options() as $value) {
+					if ($start)
+						$new_options[] = $value;
+
+					if ($value['type'] == 'open_ajax' && $value['id'] == $_POST['ajax'])
+						$start = true;
+
+					if ($value['type'] == 'close_ajax' && $value['id'] == $_POST['ajax'] . "_close")
+						break;
+				}
+
+				$flb->construct_form($new_options);
 			}
-			
-			$flb->construct_form($new_options);
 		break;
 		
 	case 'update_option' :
-			update_option( $_POST['id'], $_POST['value'] );
+		if(validateInput( $_POST['id'])) {
+			update_option($_POST['id'], $_POST['value']);
+		}
 		break;
 		
 	case 'delete_option' :
-			delete_option( $_POST['id'] );
+		if(validateInput($_POST['id'])) {
+			delete_option($_POST['id']);
+		}
 		break;
 }
 
